96ae3e41533c851ae1ff1a882064ebbf701068e6
Abdul Aleem <ma.aleem20@gmail.com>
2023-05-28 19:06:19 +0400

    commint1

--- mongo.sh
+++ mongo.sh
@@ -6,7 +6,6 @@ systemctl start mongod
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 
-
 systemctl restart mongod
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
