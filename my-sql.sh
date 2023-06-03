9c5a313b4e5df4f30201568df8f89ef33e5114a0
Abdul Aleem <ma.aleem20@gmail.com>
2023-06-03 13:10:41 +0400

    commint1

--- my-sql.sh
+++ my-sql.sh
@@ -3,7 +3,9 @@ dnf module disable mysql
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
-grep temp /var/log/mysqld.log
-#ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
-#SET PASSWORD FOR 'root'@'localhost' = PASSWORD 'RoboShop@1';
-#ALTER USER 'root'@'localhost' IDENTIFIED WITH 'RoboShop@1';
+
+echo ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'; > /tmp/sql_root_passwd
+DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+cat /tmp/sql_root_passwd | mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"
+
+
--- variables.sh
+++ variables.sh
@@ -2,3 +2,9 @@ VARIABLE=DATA
 
 a=10
 echo A - $a
+
+#Command Substitution
+
+VARNAME=$(command)
+
+echo $VARNAME
