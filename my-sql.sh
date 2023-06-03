41a4b58dbd9e70787c0ac30c521cccb1a93dce5a
Abdul Aleem <ma.aleem20@gmail.com>
2023-06-03 11:26:35 +0400

    commint1

--- my-sql.sh
+++ my-sql.sh
@@ -4,4 +4,5 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 grep temp /var/log/mysqld.log
-"ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';"
+#ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');
