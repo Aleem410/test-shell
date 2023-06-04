2b061d16766f8c04c612addc83b6a4669418a32c
Abdul Aleem <ma.aleem20@gmail.com>
2023-06-04 12:34:37 +0400

    commint1

--- my-sql.sh
+++ my-sql.sh
@@ -1,3 +1,9 @@
+if [ -z "$1" ]
+then
+  echo PASSWORD is required
+  exit
+fi
+
 my_sql_password=$1
 
 STAT() {
@@ -9,11 +15,6 @@ STAT() {
     exit
   fi
 }
-if [ -z "$1" ]
-then
-  echo PASSWORD is required
-  exit
-fi
 
 echo dowmload mysql repo
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
