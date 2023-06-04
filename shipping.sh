409b211a20f3b28fd276f3febf957a3572d47e1e
Abdul Aleem <ma.aleem20@gmail.com>
2023-06-04 22:17:23 +0400

    commint

---
+++ shipping.sh
@@ -0,0 +1,23 @@
+source common.sh
+COMPONENT=shipping
+
+PRINT "INSTALL MAVEN"
+yum install maven -y
+STAT $?
+
+PRINT "ADDING USER"
+ if [ $? -ne 0 ]; then
+  useradd roboshop
+ fi
+  STAT $?
+
+APP_LOC=/home/roboshop
+DOWNLOAD_APP_CODE
+
+#cd /home/roboshop
+#$ curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
+#$ unzip /tmp/shipping.zip
+$ mv $COMPONENT-main $COMPONENT
+$ cd $COMPONENT
+$ mvn clean package
+$ mv target/shipping-1.0.jar shipping.jar
