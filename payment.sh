4755fe856a6f330164b09fa99d804166b503fa19
Abdul Aleem <ma.aleem20@gmail.com>
2023-06-06 14:31:05 +0400

    commint

--- payment.sh
+++ payment.sh
@@ -11,8 +11,8 @@ unzip /tmp/payment.zip
  cd /home/roboshop/payment
  pip3 install -r requirements.txt
 
-USER_ID=${id u roboshop}
-GROUP_ID=${id g roboshop}
+USER_ID=${id -u roboshop}
+GROUP_ID=${id -g roboshop}
 
 
  sed -i -e "/uid c uid = ${USER_ID}/" -e "/uid c gid = ${GROUP_ID}/" /home/robooshop/payment/payment.ini
