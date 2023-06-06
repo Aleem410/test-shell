cc9643ea47561eec215b3350973d193ef08239e7
Abdul Aleem <ma.aleem20@gmail.com>
2023-06-06 13:22:27 +0400

    commint

--- rabbitMQ.sh
+++ rabbitMQ.sh
@@ -1,11 +1,31 @@
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
+SOURCE common.sh
+COMPONENT=rabbitmq
+RABBITMQ_PASSWORD=$1
 
+PRINT "DOWNLOAD REPO FILE"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
+STAT $?
+
+PRINT "INSTALL ERLANG"
 yum install erlang -y
+STAT $?
+
+PRINT "DOWNLOAD MAVEN REPO"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+STAT $?
+
+PRINT "INSTALL RABBITMQ"
 yum install rabbitmq-server -y
+STAT $?
+
+PRINT "ENABLE RABBITMQ"
 systemctl enable rabbitmq-server
+STAT $?
+
+PRINT "START RABBITMQ"
 systemctl start rabbitmq-server
-exit
- rabbitmqctl add_user roboshop roboshop123
+STAT $?
+
+ rabbitmqctl add_user roboshop ${RABBITMQ_PASSWORD}
 rabbitmqctl set_user_tags roboshop administrator
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
