fca8eac2790350aa6f54a952743d50596fd5c860
Abdul Aleem <ma.aleem20@gmail.com>
2023-06-06 13:26:02 +0400

    commint

---
+++ payment.sh
@@ -0,0 +1,9 @@
+yum install python36 gcc python3-devel -y
+useradd roboshop
+cd /home/roboshop
+ curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
+unzip /tmp/payment.zip
+ mv payment-main payment
+ curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
+ unzip /tmp/payment.zip
+ mv payment-main payment
