ef2da643612dd219ee6460e610799e49cbf0d098
Abdul Aleem <ma.aleem20@gmail.com>
2023-06-06 14:27:21 +0400

    commint

--- payment.sh
+++ payment.sh
@@ -9,3 +9,12 @@ unzip /tmp/payment.zip
  mv payment-main payment
  cd /home/roboshop/payment
  pip3 install -r requirements.txt
+
+USER_ID=${id u roboshop}
+GROUP_ID=${id g roboshop}
+
+
+ sed -i -e "/uid c uid = ${USER_ID}/" -e "/uid c gid = ${GROUP_ID}/" /home/robooshop/payment/payment.ini
+
+ sed -i -e 's/CARTHOST/172.31.66.198/' -e 's/USERHOST/172.31.22.94/' -e 's/USERHOST/172.31.22.94/' -e 's/AMQPHOST/172.31.86.220/' /etc/redis.conf /etc/redis/redis.conf
+
